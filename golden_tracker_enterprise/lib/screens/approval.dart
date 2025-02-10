3dcfdab5e25c9c7ea3bb17ec0a18a6388530de85
hannah.d <dasal.hannah@goldentopper.com>
2025-02-10 13:23:43 +0800

    feat[screens]: create dashboard and approval screen within dashboard

---
+++ golden_tracker_enterprise/lib/screens/approval.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class ApprovalScreen extends StatelessWidget {
+  const ApprovalScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Text('Approval Screen'),
+      ),
+    );
+  }
+}
